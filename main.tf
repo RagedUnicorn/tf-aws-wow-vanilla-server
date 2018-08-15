1218a49ab634d1480b4ce1f5c5e0f614fd57828b
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2018-08-15 17:51:40 +0200

    Pin terraform provider versions

--- main.tf
+++ main.tf
@@ -17,9 +17,17 @@ terraform {
 # AWS provider
 ###############
 provider "aws" {
+  version    = "~> 1.31"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.aws_region}"
+}
+
+####################
+# Template provider
+####################
+provider "template" {
+  version = "~> 1.0"
 }
 
 ###################################
