0c699cb6fc79f7449345d290094722ddaef6e926
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2019-04-05 21:02:08 +0200

    Deactivate cloudwatch rules for automatic stop and start

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -3,30 +3,30 @@
 ###################
 
 # GMT -2 effective time 23:55
-resource "aws_cloudwatch_event_rule" "stop_instance" {
-  name                = "rg-tf-wow-vanilla-server-ec2-stop-instance"
-  description         = "Stop instances nightly"
-  schedule_expression = "cron(55 21 * * ? *)"
-}
+# resource "aws_cloudwatch_event_rule" "stop_instance" {
+#   name                = "rg-tf-wow-vanilla-server-ec2-stop-instance"
+#   description         = "Stop instances nightly"
+#   schedule_expression = "cron(55 21 * * ? *)"
+# }
 
 # GMT -2 effective time 17:05
-resource "aws_cloudwatch_event_rule" "start_instance" {
-  name                = "rg-tf-wow-vanilla-server-ec2-start-instance"
-  description         = "Start instances in the evening"
-  schedule_expression = "cron(5 15 * * ? *)"
-}
+# resource "aws_cloudwatch_event_rule" "start_instance" {
+#   name                = "rg-tf-wow-vanilla-server-ec2-start-instance"
+#   description         = "Start instances in the evening"
+#   schedule_expression = "cron(5 15 * * ? *)"
+# }
 
 ####################
 # Cloudwatch target
 ####################
-resource "aws_cloudwatch_event_target" "stop_instance" {
-  rule  = "${aws_cloudwatch_event_rule.stop_instance.name}"
-  arn   = "${aws_lambda_function.start_stop_instance.arn}"
-  input = "{\"action\": \"stop\",\"region\": \"${var.aws_region}\",\"instanceId\": \"${module.server.id}\"}"
-}
+# resource "aws_cloudwatch_event_target" "stop_instance" {
+#   rule  = "${aws_cloudwatch_event_rule.stop_instance.name}"
+#   arn   = "${aws_lambda_function.start_stop_instance.arn}"
+#   input = "{\"action\": \"stop\",\"region\": \"${var.aws_region}\",\"instanceId\": \"${module.server.id}\"}"
+# }
 
-resource "aws_cloudwatch_event_target" "start_instance" {
-  rule  = "${aws_cloudwatch_event_rule.start_instance.name}"
-  arn   = "${aws_lambda_function.start_stop_instance.arn}"
-  input = "{\"action\": \"start\",\"region\": \"${var.aws_region}\",\"instanceId\": \"${module.server.id}\"}"
-}
+# resource "aws_cloudwatch_event_target" "start_instance" {
+#   rule  = "${aws_cloudwatch_event_rule.start_instance.name}"
+#   arn   = "${aws_lambda_function.start_stop_instance.arn}"
+#   input = "{\"action\": \"start\",\"region\": \"${var.aws_region}\",\"instanceId\": \"${module.server.id}\"}"
+# }
--- lambda.tf
+++ lambda.tf
@@ -11,19 +11,19 @@ resource "aws_lambda_function" "start_stop_instance" {
   timeout          = 60
 }
 
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_start" {
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.start_stop_instance.function_name}"
-  principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.start_instance.arn}"
-}
+# resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_start" {
+#   action        = "lambda:InvokeFunction"
+#   function_name = "${aws_lambda_function.start_stop_instance.function_name}"
+#   principal     = "events.amazonaws.com"
+#   source_arn    = "${aws_cloudwatch_event_rule.start_instance.arn}"
+# }
 
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_stop" {
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.start_stop_instance.function_name}"
-  principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.stop_instance.arn}"
-}
+# resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_stop" {
+#   action        = "lambda:InvokeFunction"
+#   function_name = "${aws_lambda_function.start_stop_instance.function_name}"
+#   principal     = "events.amazonaws.com"
+#   source_arn    = "${aws_cloudwatch_event_rule.stop_instance.arn}"
+# }
 
 resource "aws_lambda_permission" "allow_api_gateway_to_call_lambda" {
   action        = "lambda:InvokeFunction"
